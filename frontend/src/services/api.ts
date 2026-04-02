178fe74ebe0b34920369bf7c18235c8f3c79bc7b
dszfernandezz <augustoelric@gmail.com>
2026-04-02 17:07:34 -0300

    ajustando conexao com a api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,19 +1,29 @@
 import { z } from 'zod';
-const BASE_URL = 'https://apiinterview.threepixels.com.br/api/v1';
+const BASE_URL = 'http://localhost:3000/api';
 
-export const SignupSchema = z.object ({
-    name: z.string().min(1, 'nome muito curto'),
-    email: z.string().email('Email invalido!'),
-    phone: z.string().regex(/^\d{10,11}$/, 'telefone inválido'),
-    password: z.string().min(6, 'a senha tem que ter no minimo 6 caracteres')
+export const SignupSchema = z.object({
+    nome: z.string().min(3, 'Nome muito curto'),
+    email: z.string().email('E-mail inválido'),
+    cpf: z.string().length(11, 'O CPF deve ter 11 números'), // Lembre de limpar a máscara antes de validar aqui ou no backend
+    data_nascimento: z.string().min(1, 'Data é obrigatória'),
+    telefone: z.string().min(10, 'Telefone inválido'),
+    endereco: z.string().min(5, 'Endereço muito curto'),
+    complemento: z.string().optional(),
+    cep: z.string().length(8, 'CEP deve ter 8 números'),
+    senha: z.string().min(8, 'A senha deve ter no mínimo 8 caracteres'),
+    confirmarSenha: z.string()
+}).refine((data) => data.senha === data.confirmarSenha, {
+    message: "As senhas não coincidem",
+    path: ["confirmarSenha"],
 });
 
 export type SignupData = z.infer<typeof SignupSchema>;
 
 export interface User {
-  name: string;
+  nome: string;
   email: string;
-  phone: string;
+  telefone: string;
+  cpf: string;
   [key: string]: unknown;
 }
 
@@ -29,36 +39,8 @@ export interface PreSigninResponse {
 
 /* Signup */
 
-export const checkEmailRequest = async (email: string): Promise<{ available: boolean }> => {
-    const response  = await fetch(`${BASE_URL}/authenticate/check/email`, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({ email} ),
-    });
-
-    const data = await response.json();
-
-    if(!response.ok) throw new Error(data?.message || 'o email já está em uso');
-
-    return data;
-}
-
-export const checkPhoneRequest = async (phone: string): Promise<{ available: boolean }> => {
-    const response  = await fetch(`${BASE_URL}/authenticate/check/phone`, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({ phone }),
-    });
-
-    const data = await response.json();
-
-    if(!response.ok) throw new Error(data?.message ||'o celular já está em uso');
-
-    return data;
-}   
-
 export const signupRequest = async (userData: SignupData): Promise<AuthResponse> => {
-    const response = await fetch(`${BASE_URL}/authenticate/signup`, {
+    const response = await fetch(`${BASE_URL}/register`, {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(userData),
@@ -72,20 +54,8 @@ export const signupRequest = async (userData: SignupData): Promise<AuthResponse>
 
 /* Signin */
 
-export const preSigninRequest = async (username: string): Promise<PreSigninResponse> => {
-    const response = await fetch(`${BASE_URL}/authenticate/pre/signin`, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({ username }),
-    });
-
-    if(!response.ok) throw new Error('O usuário não foi encontrado!');
-
-    return response.json();
-}
-
 export const signinRequest = async (username: string, password: string): Promise<AuthResponse>  => {
-    const response = await fetch(`${BASE_URL}/authenticate/signin`, {
+    const response = await fetch(`${BASE_URL}/register`, {
         method: "POST",
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({username, password}),
